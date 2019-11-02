e0c45c6093bdc227613d5bd504d803421d871e8f
mirannaalina <v_mxa@yahoo.com>
2019-11-02 18:20:30 +0200

    First step implementation menu

---
+++ src/Project/Comanda.java
@@ -0,0 +1,28 @@
+package Project;
+
+public class Comanda {
+
+    static boolean flag=false;
+
+   // Utilizator getNume;
+   // Optiune getOptiune(...);
+
+    public static void adaugareComanda(){
+        //...
+    }
+    public static void stergeComanda(){
+       //...
+    }
+
+    public static void setareFlagPlatit() {
+        //if(Utilizator==platit){
+        flag = true;
+
+    }
+
+    public static void vizualizareSumePerUser(){
+        //...
+    }
+}
+
+
